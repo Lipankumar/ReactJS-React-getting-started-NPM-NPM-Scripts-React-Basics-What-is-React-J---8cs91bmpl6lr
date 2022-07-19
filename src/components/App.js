9035dce9bae52d57bdb90ec5d7948d20fbb908c4
Lipankumar <104409165+Lipankumar@users.noreply.github.com>
2022-07-19 10:22:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
-import  Heading from "./components/Heading";
+import  Heading from "./Heading";
 import  InputQuery from "./components/InputQuery";
 import  SubHeading from "./components/SubHeading";
 import  SubmitButton from "./components/SubmitButton";
